7184887bfc17e0e0684b6548b409394e8d64e7ed
zhuozhizheng <51820540+zhuozhizheng@users.noreply.github.com>
2024-05-10 17:07:04 +0800

    Diffusion_Data_Preprocess.sh

--- Diffusion_image_processing_and_NODDI_metric_calculation/Diffusion_Data_Preprocess.sh
+++ Diffusion_image_processing_and_NODDI_metric_calculation/Diffusion_Data_Preprocess.sh
@@ -7,26 +7,28 @@ echo $i
 
 cd $i
 
-# note: this additional code is for the NODDI data that with reverse phase-encoding 
-#fslroi NODDI_AP.nii.gz AP 0 1
-#fslroi NODDI_PA.nii.gz PA 0 1
-#fslmerge -t AP_PA AP.nii.gz PA.nii.gz
-#topup --imain=AP_PA.nii.gz --datain=acq_param.txt --config=b02b0.cnf --out=AP_PA_topup
-#applytopup --imain=NODDI_AP.nii.gz --inindex=1 --datain=acq_param.txt --topup=AP_PA_topup --method=jac --out=AP_cor
-#fslroi AP_cor AP_1stVol 0 1
-#bet AP_1stVol.nii.gz AP_brain -m -f 0.2
-#cp AP_cor.nii.gz data.nii.gz
-
-
-eddy_correct data.nii.gz DATA.nii.gz 0 
 cp *.bvals.bval bvals.bval
 cp *.bvecs.bvec bvecs.bvec
 
-dt_rotate_bvecs $i/bvecs.bvec $i/rotate_bvecs.bvec $i/DATA.ecclog
+fslroi NODDI_AP.nii.gz AP 0 1
+fslroi NODDI_PA.nii.gz PA 0 1
+fslmerge -t AP_PA AP.nii.gz PA.nii.gz
+topup --imain=AP_PA.nii.gz --datain=acq_param.txt --config=b02b0.cnf --out=AP_PA_topup
+applytopup --imain=NODDI_AP.nii.gz --inindex=1 --datain=acq_param.txt --topup=AP_PA_topup --method=jac --out=AP_cor
+fslroi AP_cor AP_1stVol 0 1
+bet AP_1stVol.nii.gz AP_brain -m -f 0.2
+cp AP_cor.nii.gz data.nii.gz
 
-fslroi DATA.nii.gz nodif.nii.gz 0 1
+indx=""
+for ((i=1; i<=48; i+=1)); do indx="$indx 1"; done
+echo $indx > index.txt
 
-bet nodif.nii.gz nodif_brain.nii.gz -m -f 0.2
+eddy --imain=data.nii.gz --mask=AP_brain_mask --acqp=acq_param.txt --index=index.txt --bvecs=bvecs --bvals=bvals --topup=AP_PA_topup --out=eddy_corrected_data
+
+cp eddy_corrected_data.nii.gz DATA.nii.gz
+
+cp eddy_corrected_data.rotated_bvecs bvecs.bvec
 
 cd ..
 done
+
